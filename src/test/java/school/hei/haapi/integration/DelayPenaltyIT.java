291a0644037eb26abb1dd3a1b6caf5075220f780
dinasoa <hei.dinasoa@gmail.com>
2023-04-02 10:31:49 +0300

    feat: add crupdate delayPenalty test

--- src/test/java/school/hei/haapi/integration/DelayPenaltyIT.java
+++ src/test/java/school/hei/haapi/integration/DelayPenaltyIT.java
@@ -2,17 +2,25 @@ package school.hei.haapi.integration;
 
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import school.hei.haapi.SentryConf;
+import school.hei.haapi.endpoint.rest.api.PayingApi;
 import school.hei.haapi.endpoint.rest.client.ApiClient;
+import school.hei.haapi.endpoint.rest.client.ApiException;
+import school.hei.haapi.endpoint.rest.model.CreateDelayPenaltyChange;
+import school.hei.haapi.endpoint.rest.model.DelayPenalty;
 import school.hei.haapi.endpoint.rest.security.cognito.CognitoComponent;
 import school.hei.haapi.integration.conf.AbstractContextInitializer;
 import school.hei.haapi.integration.conf.TestUtils;
+import static org.junit.Assert.assertNotEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static school.hei.haapi.integration.conf.TestUtils.MANAGER1_TOKEN;
+import static school.hei.haapi.integration.conf.TestUtils.STUDENT1_TOKEN;
 import static school.hei.haapi.integration.conf.TestUtils.anAvailableRandomPort;
 import static school.hei.haapi.integration.conf.TestUtils.setUpCognito;
 
@@ -29,11 +37,29 @@ class DelayPenaltyIT {
   private static ApiClient anApiClient(String token) {
     return TestUtils.anApiClient(token, DelayPenaltyIT.ContextInitializer.SERVER_PORT);
   }
+
+  public CreateDelayPenaltyChange toCrupdate(){
+    return new CreateDelayPenaltyChange()
+        .interestPercent(5)
+        .graceDelay(20)
+        .interestTimerate(CreateDelayPenaltyChange.InterestTimerateEnum.DAILY)
+        .applicabilityDelayAfterGrace(5);
+  }
   @BeforeEach
   void setUp() {
     setUpCognito(cognitoComponentMock);
   }
 
+  @Test
+  void crupdate_delay_penalty_ok() throws ApiException{
+    ApiClient manager1CLient = anApiClient(MANAGER1_TOKEN);
+    PayingApi api = new PayingApi(manager1CLient);
+
+    DelayPenalty actual = api.getDelayPenalty();
+    DelayPenalty actualUpdated = api.createDelayPenaltyChange(toCrupdate());
+
+    assertNotEquals(actual, actualUpdated);
+  }
   static class ContextInitializer extends AbstractContextInitializer {
     public static final int SERVER_PORT = anAvailableRandomPort();
 
