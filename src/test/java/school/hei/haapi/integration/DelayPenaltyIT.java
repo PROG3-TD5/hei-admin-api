c368999e9fff28e0f74194eea5081a2aaca496b6
Steven1805 <hei.steven.2@gmail.com>
2023-04-02 15:49:38 +0300

    feat: add DelayPenalty integration test

--- src/test/java/school/hei/haapi/integration/DelayPenaltyIT.java
+++ src/test/java/school/hei/haapi/integration/DelayPenaltyIT.java
@@ -1,7 +1,7 @@
 package school.hei.haapi.integration;
 
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -11,11 +11,10 @@ import school.hei.haapi.SentryConf;
 import school.hei.haapi.endpoint.rest.api.PayingApi;
 import school.hei.haapi.endpoint.rest.client.ApiClient;
 import school.hei.haapi.endpoint.rest.client.ApiException;
-import school.hei.haapi.endpoint.rest.model.CreateDelayPenaltyChange;
+import school.hei.haapi.endpoint.rest.model.DelayPenalty;
 import school.hei.haapi.endpoint.rest.security.cognito.CognitoComponent;
 import school.hei.haapi.integration.conf.AbstractContextInitializer;
 import school.hei.haapi.integration.conf.TestUtils;
-import school.hei.haapi.model.DelayPenalty;
 
 import java.time.Instant;
 
@@ -38,23 +37,13 @@ class DelayPenaltyIT {
     return TestUtils.anApiClient(token, DelayPenaltyIT.ContextInitializer.SERVER_PORT);
   }
 
-  public static DelayPenalty delayPenalty() {
-    DelayPenalty delayPenalty = new DelayPenalty();
-    delayPenalty.setId("delay_penalty_id1");
-    delayPenalty.setCreationDatetime(Instant.parse("2021-11-01T09:36:37.00Z"));
-    delayPenalty.setInterestPercent(0);
-    delayPenalty.setInterestTimerate(DAILY);
-    delayPenalty.setGraceDelay(0);
-    delayPenalty.setApplicabilityDelayAfterGrace(0);
-    return delayPenalty;
-  }
-
-  private static CreateDelayPenaltyChange createDelayPenaltyChange() {
-    return new CreateDelayPenaltyChange()
-            .interestPercent(0)
-            .interestTimerate(school.hei.haapi.endpoint.rest.model.CreateDelayPenaltyChange.InterestTimerateEnum.DAILY)
-            .graceDelay(0)
-            .applicabilityDelayAfterGrace(0);
+  public static DelayPenalty delayPenalty2() {
+    return new DelayPenalty()
+        .id("delay_penalty1_id")
+        .interestPercent(0)
+        .interestTimerate(DAILY)
+        .graceDelay(0)
+        .applicabilityDelayAfterGrace(0);
   }
 
   @BeforeEach
@@ -63,13 +52,13 @@ class DelayPenaltyIT {
   }
 
   @Test
-  void student_read_ok() throws ApiException {
-    ApiClient student1Client = anApiClient(STUDENT1_TOKEN);
-    PayingApi api = new PayingApi(student1Client);
+  void manager_read_ok() throws ApiException {
+    ApiClient manager1Client = anApiClient(MANAGER1_TOKEN);
+    PayingApi api = new PayingApi(manager1Client);
 
-    school.hei.haapi.endpoint.rest.model.DelayPenalty actualDelayPenalty = api.getDelayPenalty();
+    DelayPenalty actualDelayPenalty = api.getDelayPenalty().creationDatetime(null);
 
-    assertEquals(delayPenalty(), actualDelayPenalty);
+    assertEquals(actualDelayPenalty, delayPenalty2());
   }
 
   static class ContextInitializer extends AbstractContextInitializer {
